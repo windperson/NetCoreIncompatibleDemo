179ef00e66475c8c3137b1a22ea808a1b8d1d5e5
Chen Yu Pao <windperson@gmail.com>
2018-06-29 21:59:57 +0800

    Demo code to show APM async operation is not support in .Net Core

---
+++ DemoOldAPM/OldApmDemo.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace DemoOldAPM
+{
+    public class OldApmDemo
+    {
+        private delegate int AsyncDelegate(int input, TimeSpan waitTimeSpan);
+        public void DoOldApm()
+        {
+            try
+            {
+                AsyncDelegate asyncDelegate = AsyncWaitThenReturn;
+
+                IAsyncResult asyncResult = asyncDelegate.BeginInvoke(123, new TimeSpan(0, 0, 1), ar =>
+                {
+                    var result = asyncDelegate.EndInvoke(ar);
+                    Console.WriteLine("result=" + result);
+                }, null);
+
+                SpinWait.SpinUntil(() => asyncResult.IsCompleted);
+
+                Console.WriteLine("Async Delegate Finished.");
+
+            }
+            catch (PlatformNotSupportedException ex)
+            {
+                Console.WriteLine("PlatformNotSupportedException=" + ex);
+            }
+        }
+
+        private static int AsyncWaitThenReturn(int x, TimeSpan waitTime)
+        {
+            Task.Delay(waitTime).Wait();
+            return x;
+        }
+    }
+}
--- NefFrameworkConsole/Program.cs
+++ NefFrameworkConsole/Program.cs
@@ -1,13 +1,17 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 using DemoOldAPM;
 
-namespace NetCoreIncompatibleDemo
+namespace NefFrameworkConsole
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Press any key to begin trying Old Delgate based APM operation on .Net Core");
+            Console.WriteLine("Press any key to begin trying Old Delgate based APM operation on .Net Framework");
             Console.ReadKey();
             var oldApm = new OldApmDemo();
 
