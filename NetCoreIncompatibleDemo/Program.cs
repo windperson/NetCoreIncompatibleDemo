466d0d0ea3a1a2f3aaf78f1562c61fba5f985b6e
Chen Yu Pao <windperson@gmail.com>
2018-06-29 21:13:27 +0800

    Add project files.

---
+++ NetCoreIncompatibleDemo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NetCoreIncompatibleDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
