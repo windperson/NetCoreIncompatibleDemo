3cbbce2eaabcba2b6dd5ff44d0156361a8bb9f86
Chen Yu Pao <windperson@gmail.com>
2018-06-30 21:37:02 +0800

    add an Unity3D 2017LTS runtime demo

---
+++ Unity2017LtsDemo/UnityRuntimeDemo/Assets/DebugLogWriter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace UnityApmDemo
+{
+    public class DebugLogWriter : System.IO.TextWriter
+    {
+        public override Encoding Encoding
+        {
+            get
+            {
+                return Encoding.UTF8;
+            }
+        }
+
+        public override void Write(string value)
+        {
+            base.Write(value);
+            Debug.Log(value);
+        }
+    }
+}
---
+++ Unity2017LtsDemo/UnityRuntimeDemo/Assets/demo.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace UnityApmDemo
+{
+    public class demo : MonoBehaviour
+    {
+
+        // Use this for initialization
+        void Start()
+        {
+            System.Console.SetOut(new DebugLogWriter());
+
+            Debug.Log("Start to run shared assembly in Unity 2017 Lts with .net 3.5 compatible runtime.");
+
+            var apm = new DemoOldAPM.OldApmDemo();
+            apm.DoOldApm();
+
+            Debug.Log("Finished.");
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
+
+
